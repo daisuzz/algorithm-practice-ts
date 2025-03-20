e4ae3ab99f64a0ee8fb59999ffc13ab61789bd21
daisuzz <daisaku.rugby@gmail.com>
2025-03-20 21:42:14 +0900

    Binary Search

---
+++ 704.binary-search.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+    let startIndex = 0;
+    let endIndex = nums.length - 1;
+
+    while (startIndex <= endIndex) {
+        const middleIndex = Math.floor((startIndex + endIndex) / 2);
+        const middleValue = nums[middleIndex];
+
+        if (middleValue === target) {
+            return middleIndex;
+        }
+        else if (middleValue < target) {
+            startIndex = middleIndex + 1;
+        } else {
+            endIndex = middleIndex - 1;
+        }
+    }
+
+    return -1;
+}
+// @lc code=end
+
